8a19dca5e02b65a981008f517c9ade2d3bfc26c7
Grant Woods <grantdwoods@outlook.com>
2019-01-09 14:58:18 -0800

    Dummy log-in logic.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },  { path: 'login', loadChildren: './public/login/login.module#LoginPageModule' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren: './public/login/login.module#LoginPageModule' },
   { path: 'register', loadChildren: './public/register/register.module#RegisterPageModule' },
-  { path: 'dashboard', loadChildren: './users/dashboard/dashboard.module#DashboardPageModule' },
+  //{ path: 'dashboard', loadChildren: './users/dashboard/dashboard.module#DashboardPageModule' },
 
 ];
 
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Platform } from '@ionic/angular';
+
+const JWT = 'jwt';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthenticationService {
+export class AuthenticationService 
+{
+  authenticationState = new BehaviorSubject(false);
+  
+  constructor(private plt: Platform, private storage: Storage) { 
+    this.plt.ready().then(()=> {
+      this.checkToken();
+    })
+  }
 
-  constructor() { }
+  login(){
+    return this.storage.set(JWT, 'sampletoken').then(res => {
+      this.authenticationState.next(true);
+    });
+  }
+
+  //alternative promise syntax, if we dont need information returned. 
+  logout(){
+    return this.storage.remove(JWT).then(()=> {
+      this.authenticationState.next(false);
+    });
+  }
+
+  isAuthenticated(){
+    return this.authenticationState.value;
+  }
+  //ask the backend if the token is good
+  checkToken(){
+    return this.storage.get(JWT).then(res => {
+      if(res){
+        this.authenticationState.next(true);
+      }
+    });
+  }
 }
