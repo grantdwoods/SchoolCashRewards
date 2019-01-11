ef02b0f005a036fcce453f0118ae3d934ed6d85a
Grant Woods <grantdwoods@outlook.com>
2019-01-11 14:31:53 -0800

    Added form and submit button to login page

--- src/app/public/login/login.page.ts
+++ src/app/public/login/login.page.ts
@@ -8,15 +8,25 @@ import { AuthenticationService } from '../../services/authentication.service';
 })
 export class LoginPage implements OnInit {
 
+  private showForm: boolean;
+  private username: string;
+  private password: string;
+
   constructor(private authService: AuthenticationService) { }
 
   ngOnInit() {
+    this.showForm = false;
   }
 
+  toggleForm(){
+    this.showForm = true;
+  }
   //display feilds for login? 
   login(){
-    console.log('Loggin in....');
-    this.authService.login();
+    console.log(this.username + ' ' + this.password);
+    console.log('Attempting log in....');
+    
+    //this.authService.login();
   }
 
 }
