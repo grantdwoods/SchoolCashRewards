5bacc5241d4c77722156486ac656e019b55e9289
Grant Woods <grantdwoods@outlook.com>
2019-01-18 14:50:34 -0800

    Blank pages for catalog and student

--- src/app/users/user-routing.module.ts
+++ src/app/users/user-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
  
 const routes: Routes = [
-  { path: 'class', loadChildren: './class/class.module#ClassPageModule' }
+  { path: 'class', loadChildren: './class/class.module#ClassPageModule' },  { path: 'catalog', loadChildren: './catalog/catalog.module#CatalogPageModule' },
+  { path: 'student', loadChildren: './student/student.module#StudentPageModule' }
+
 ];
  
 @NgModule({
