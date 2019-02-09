be72587084235ab81db304440ea852cbeeee91ea
Ryan <rpointer@eagles.ewu.edu>
2019-02-08 18:11:20 -0800

    Added modal for awarding coupons

---
+++ src/app/users/user-tabs/student/student-info/award-modal/award-modal.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-award-modal',
+  templateUrl: './award-modal.page.html',
+  styleUrls: ['./award-modal.page.scss'],
+})
+export class AwardModalPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
