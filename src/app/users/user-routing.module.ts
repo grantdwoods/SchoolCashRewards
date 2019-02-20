b2c368e93da8000d7dcbbcf5c630954c83ec4400
Grant Woods <grantdwoods@outlook.com>
2019-02-20 14:20:30 -0800

    meep

--- src/app/users/user-routing.module.ts
+++ src/app/users/user-routing.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
  
 const routes: Routes = [
-  { path: 'user-tabs', loadChildren: './user-tabs/user-tabs.module#UserTabsPageModule' },  { path: 'add-class', loadChildren: './user-tabs/class/add-class/add-class.module#AddClassPageModule' }
-
-];
   { path: 'user-tabs', loadChildren: './user-tabs/user-tabs.module#UserTabsPageModule' },
+  { path: 'add-class', loadChildren: './user-tabs/class/add-class/add-class.module#AddClassPageModule' },
   { path: 'award-modal', loadChildren: './user-tabs/student/student-info/award-modal/award-modal.module#AwardModalPageModule' }
 
 ];
