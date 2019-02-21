d5df562e6646559763dcb7d07cc36bdeb9c43dd5
Grant Woods <grantdwoods@outlook.com>
2019-02-20 21:17:13 -0800

    new school almost working
    need to fix some back-end stuff...

--- src/app/services/registration.service.ts
+++ src/app/services/registration.service.ts
@@ -8,26 +8,42 @@ import { FormGroup } from '@angular/forms';
 })
 export class RegistrationService {
 
-  constructor(private http:HttpClient, private authService:AuthenticationService) {
-   }
 
-   registerAccountWithAuth(formGroup: FormGroup, newSchool:number){
-    var formData = new FormData();
-    formData.append('userID', formGroup.value['userID']);
-    formData.append('password', formGroup.value['password']);
-    formData.append('role', 't');
-    formData.append('schoolID', formGroup.value['schoolID']);
-    formData.append('newSchool', newSchool.toString());
+  constructor(private http:HttpClient, private authService:AuthenticationService) {}
 
-    return this.http.post(this.authService.BASEURL + 'registerUser.php',formData,{});
-   }
+  registerAccountWithAuth(formGroup: FormGroup, newSchool:boolean){
+  let registerNewSchool = newSchool ? 1:0;
+  let role = newSchool ? 'a': 't';
 
-   registerAccountWithApp(formGroup: FormGroup){
-    var formData = new FormData();
-    formData.append('firstName', formGroup.value['firstName']);
-    formData.append('lastName', formGroup.value['lastName']);
+  var formData = new FormData();
+  formData.append('userID', formGroup.value['userID']);
+  formData.append('password', formGroup.value['password']);
+  formData.append('role', role);
+  formData.append('schoolID', formGroup.controls['schoolID'].value);
+  formData.append('newSchool', registerNewSchool.toString());
 
-    return this.http.post('teachers.php', formData,{});
-   }
+  return this.http.post(this.authService.BASEURL + 'registerUser.php',formData,{});
+  }
+
+  registerAccountWithApp(formGroup: FormGroup){
+  var formData = new FormData();
+  formData.append('firstName', formGroup.value['firstName']);
+  formData.append('lastName', formGroup.value['lastName']);
+
+  return this.http.post('teachers.php', formData, {});
+  }
+
+  //schoolID is pulled out of JWT in schools.php
+  registerSchoolWithApp(formGroup: FormGroup){
+    let formData = new FormData();
+    formData.append('schoolName', formGroup.value['schoolName']);
+    formData.append('cashName', formGroup.value['schoolCashName']);
+
+    return this.http.post('schools.php', formData, {});
+  }
+  
+  checkForExistingSchool(schoolCode:number){
+    return this.http.get(this.authService.BASEURL + 'registerUser.php?schoolID=' + schoolCode);
+  }
    
 }
