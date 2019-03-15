15829121492315fd030df0d02534d28844d8e0e9
Grant Woods <grantdwoods@outlook.com>
2019-03-15 09:14:03 -0700

    settings page

--- src/app/users/user-routing.module.ts
+++ src/app/users/user-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
  
 const routes: Routes = [
   { path: 'user-tabs', loadChildren: './user-tabs/user-tabs.module#UserTabsPageModule' },
-  { path: 'award-modal', loadChildren: './user-tabs/student/student-info/award-modal/award-modal.module#AwardModalPageModule' },
+  { path: 'award-modal', loadChildren: './user-tabs/student/student-info/award-modal/award-modal.module#AwardModalPageModule' },  { path: 'settings', loadChildren: './user-tabs/settings/settings.module#SettingsPageModule' },
+
 
 ];
  
